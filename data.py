3d13ef905761ed40e45c4d8d8d9e5d9b489cb53e
elisa-23 <elisac40@nycstudents.net>
2024-02-05 12:29:52 -0500

    a

---
+++ data.py
@@ -0,0 +1,41 @@
+#Data Types
+#Numbeers 1,2,3
+""" def add(x,y):
+    print(x+y)
+add(1,2)
+#strings "a,b,c"
+name = "Mark"
+def greeting(person):
+    print(person)
+greeting(name)
+#1 and "1" are not the same
+add("1","2")
+#undefined/null
+
+#booleans
+tenure = False
+def is_tenured(status):
+    if(status==true):
+        print("They have tenured")
+    else:
+        print("they are not tenured") """
+
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+print(values[0])
+print(values[6]) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+def input(_type a random sentence: )
