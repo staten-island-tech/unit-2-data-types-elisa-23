cd0f9caee9bfea2ffa91298bb8924ba8748208e2
elisa-23 <elisac40@nycstudents.net>
2024-02-09 12:29:24 -0500

    d

--- data.py
+++ data.py
@@ -89,7 +89,27 @@ def factors():
         else:
             y += 1
             number % y
-    print(factor)
-factors() """
-        
-        
+    print(f"Factor:{factor}") """
+
+factor1 = [1]
+factor2 = []
+def gcf():
+    number1 = int(input("Enter your first number: "))
+    number2 = int(input("Enter your second number: "))
+    y = 1
+    for i in range(number1):
+        y += 1
+        if number1 % y == 0:
+            if number2/y == 0:
+                if y>factor2[1]:
+                    factor2 = []
+                    factor2.append(y)
+            if number2/(number1/y) == 0:
+                if (number1/y)>factor2[1]:
+                    factor2 = []
+                    factor2.append(number1/y)
+        else:
+            y += 1
+            number1 % y
+    print(f"GCF;{factor2[1]}")
+gcf()
---
+++ in.py
@@ -0,0 +1,6 @@
+pokemon = ["Charmander","Squirtle","Raichu","Gengar"]
+def check(name,mons):
+    if(name in mons):
+        print(name)
+
+check("Squirtle",pokemon)
